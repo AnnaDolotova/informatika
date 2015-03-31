6896b8e697bb4f2dbd53c06ecddc14294ee4e157
Долотова <anjutad@mail.ru>
2015-04-01 02:04:24 +0300

    Graph

---
+++ Graph/TestGraph.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include "graph.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+	int count_of_vertex;
+	cout << "Введите количество вершин графа" << endl;
+	cin >> count_of_vertex;
+
+	int a=0, b=0;
+	while (a>count_of_vertex && a!=0) {
+		cout << "Недопустимое значение" << endl;
+		cin >> a;
+	}
+	while (b>count_of_vertex && b!=0) {
+		cout << "Недопустимое значение! Попробуйте ещё." << endl;
+		cin >> b;
+	}
+
+	Graph Orlov = new Graph(count_of_vertex);
+	fill_graph();
+	print_graph_like_matrix();
+	path(a,b);
+
+	delete Orlov;
+    return 0;
+}
---
+++ Graph/graph.h
@@ -0,0 +1,77 @@
+/* graph.h */
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Graph {
+    private:
+		int number; // количество вершин в графе
+		char* matrix; // указатель на матрицу
+
+    public:
+    Graph(int count_of_vertex){  // конструктор
+		number = count_of_vertex;
+        matrix = (char*)calloc(sizeof(char), number*number);
+    }
+    
+    void fill_graph() {
+		for (int i=0; i<number; i++) {
+			for (int j=0; j<number; j++) 
+				matrix[i*number+j]=rand()%2;
+		}
+	}
+
+	void print_graph_like_matrix() {
+		for (int i=0; i<number; i++) {
+			for (int j=0; j<number; j++) 
+				cout << matrix[i*number+j];
+		};
+		cout << endl;
+	}
+
+	void path(int from, int to) {
+		bool existing = false;
+		char* path_of_vertex; // сюда будем записывать путь, по которому шли, чтобы попасть в вершину
+		path_of_vertex = (char*)calloc(sizeof(char), number);
+
+		int k = 1; // количество пройденных ребёр
+		int tmp = from; //вершина, в которой находимся в данный момент
+
+		void recursion(int k) {
+			for (int i=0; i<number; i++) {
+				if (matrix[tmp*number+i] == 1) {
+					path_of_vertex[k] = i;
+					tmp = i;
+					if (i == to) {
+						break;
+						existing = true;
+					}
+					for (int j=1; j<=k; j++) {
+						if (path_of_vertex[j] == i) {
+							break;
+							path_of_vertex[j] = 0;
+							i++;
+						}
+					}
+					k++;
+					recursion(k);
+				}
+			}
+		};
+
+		if (existing == true) {
+			cout << "Путь из вершины " << from << "в вершину " << to << ": ";
+			for (int i=0; i<number; i++) {
+				cout << ", " << path_of_vertex[i];
+			}
+		}
+		else cout << "Путь из вершины " << from << "в вершину " << to << "не существует."  << endl;
+	}
+
+
+    ~Graph() {  // деструктор
+        free(matrix);
+		cout << "Память была очищена. До свидания!" << endl;
+    }    
+}
