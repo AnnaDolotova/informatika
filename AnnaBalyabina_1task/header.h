250eecd2f617495bffcc53c1179ab0469fc7afff
Долотова <anjutad@mail.ru>
2015-04-15 00:41:25 +0300

    15/04/2015

--- AnnaBalyabina_1task/header.h
+++ AnnaBalyabina_1task/header.h
@@ -2,34 +2,98 @@
 
 using namespace std;
 
-class node {
+class Node {
+
 public:
+    Node *l, *r;
     int info;
-    node *l, *r;
-    void push(int a, node **t) {
-        if ((*t) == NULL) {
-            (*t) = new node;
-            (*t)->info = a;
-            (*t)->l = (*t)->r = NULL;
-            return;
-        }
-        if (a > (*t)->info) push(a, &(*t)->r);
-        else push(a, &(*t)->l);
+
+    Node(int i) {
+        info = i;
+        l = NULL;
+        r = NULL;
     }
-    
-    void print (node *t,int u)
-    {
-        if (t == NULL) return;
-        else
-        {
-            print(t->l,++u);
-            for (int i = 0; i < u; ++i) cout << "|";
-            cout << t->info << endl;
-            u--;
+
+    void push(int a) {
+        Node * t;
+
+        if (a < info) {
+            if (l == NULL) {
+                t = new Node(a);
+                l = t;
+            } else {
+                l->push(a);
+            }
+        } else {
+            if (r == NULL) {
+                t = new Node(a);
+                r = t;
+            } else {
+                r->push(a);
+            }
         }
-        print(t->r, ++u);
+    }
+
+    void pop() {
+        if (l == NULL) {
+            cout << "min element = " << info << endl;
+        } else {
+            l->pop();
+        }
+    }
+
+    Node * search(int num) {
+        if (info == num) {
+            return this;
+        } else {
+            if (l != NULL)
+                l->search(num);
+            if (r != NULL)
+                r->search(num);
+        }
+        return NULL;
+    }
+
+    void print(int u) {
+        if (l != NULL)
+            l->print(u + 1);
+        for (int i = 0; i < u; i++)
+            cout << "|";
+        cout << info << endl;
+        if (r != NULL)
+            r->print(u + 1);
     }
 };
 
 
+class Tree{
 
+private:
+    Node * root;
+
+public:
+    Tree(int a){
+        root = new Node(a);
+    }
+
+    Tree() {
+        root = new Node(0);
+    }
+
+    void push(int a) {
+        root->push(a);
+    }
+
+    void print(){
+        root->print(1);
+    }
+
+    void pop() {
+        root->pop();
+    }
+
+    bool search(int num){
+        Node * n = root->search(num);
+        return n == NULL ? false : true;
+    }
+};
--- AnnaBalyabina_1task/main.cpp
+++ AnnaBalyabina_1task/main.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
-#include <curses.h>
 #include "header.h"
-using namespace std ;
-
+using namespace std;
 
 
 int main ()
 {
-    node *d = NULL;
-    int n;
-    int s;
+    Tree * my_tree = NULL;
+    int n, s, k;
     cout << "vvedite kolichestvo elementov  " << endl;
     cin >> n;
-    
+
     for (int i = 0; i < n; ++i)
     {
         cout << "vvedite chislo " << endl;
         cin >> s;
-        d->push(s, &d);
-        
-    
-    }
-    cout<<"your tree"<<endl;
-    d->print(d,0);
 
+        if (my_tree == NULL)
+            my_tree = new Tree(s);
+        else
+            my_tree->push(s);
+
+    }
+    cout << "your tree" << endl;
+    my_tree->print();
+    cout << "vvedite num for search" << endl;
+    cin >> k;
+    if (my_tree->search(k))
+        cout<< k << " is in the tree" << endl;
+    else
+        cout<< k << " isn't in the tree" << endl;
+
+
+    my_tree->pop();
+    my_tree->print();
+    return 0;
 }
